28d676e615eee74704f33de5e8787a30bc7e8cad
omjiverma <omjidvlpr@gmail.com>
2022-12-12 14:50:15 +0530

    simple route setup

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express =  require('express')
 const app = express();
+const tasks = require('./routes/task')
+// Routes
+app.get('/',(req, res)=>{
+    res.send('task manager')
+})
+
+app.use('/api/v1/tasks',tasks)
 
 const PORT = 3000
 
