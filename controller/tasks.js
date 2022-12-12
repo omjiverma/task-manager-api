e77935708cad5d4ac3c6a579251dc7261631d552
omjiverma <omjidvlpr@gmail.com>
2022-12-12 16:01:22 +0530

    simple connection to mongo db

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,9 +1,27 @@
-
-
 const getAllTasks = (req, res) =>{
     res.send('all items');
 }
 
+const createTasks = (req, res) =>{
+    res.send('create a task');
+}
+
+const getTask = (req, res) =>{
+    res.send('get signle task');
+}
+
+const updateTasks = (req, res) =>{
+    res.send('update task');
+}
+
+const deleteTask = (req, res) =>{
+    res.send('delete task');
+}
+
 module.exports = {
     getAllTasks,
+    createTasks,
+    getTask,
+    updateTasks,
+    deleteTask
 }
--- routes/task.js
+++ routes/task.js
@@ -1,7 +1,8 @@
-const express =  require('express')
+const express =  require('express');
 const router = express.Router();
-const {getAllTasks} = require('../controller/tasks.js')
+const {getAllTasks, createTasks, getTask, updateTasks, deleteTask} = require('../controller/tasks.js');
 
-router.route('/').get(getAllTasks)
+router.route('/').get(getAllTasks).post(createTasks)
+router.route('/:id').get(getTask).patch(updateTasks).delete(deleteTask)
 
 module.exports = router
